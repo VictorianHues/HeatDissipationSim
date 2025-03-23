2d99ca231101d2fab3bc4beaaaab3df83589d681
VictorianHues <VictorianHues@gmail.com>
2025-03-23 19:24:57 +0100

    Diffusion grids

--- include/compute.h
+++ include/compute.h
@@ -4,7 +4,11 @@
 #include "input.h"
 #include "output.h"
 
-void do_compute(const struct parameters *p, 
+void do_compute(const struct parameters *p,
                 struct results *r);
+
+void update_results(const struct parameters *p, double **temp_grid, 
+    size_t iteration, size_t N, size_t M, struct results *r, 
+    double max_diff, time_t start_time);
 
 #endif
